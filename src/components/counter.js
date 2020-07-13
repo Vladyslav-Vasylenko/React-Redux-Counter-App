7eb6574c73bc3c060ab4942d03bd22dea982e499
Vladyslav Vasylenko <vladyslav.vasylenko@dev.pro>
2020-07-13 18:14:38 +0300

    init

---
+++ src/components/counter.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+const Counter = ({ counter, inc, dec, rnd }) => {
+  return (
+    <div className='jumbotron'>
+      <h2>{counter}</h2>
+      <button onClick={inc} className='btn btn-primary btn-large'>
+        INC
+      </button>
+      <button onClick={dec} className='btn btn-primary btn-large'>
+        DEC
+      </button>
+      <button onClick={rnd} className='btn btn-primary btn-large'>
+        RANDOM
+      </button>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({ counter: state });
+
+export default connect(mapStateToProps, actions)(Counter);
